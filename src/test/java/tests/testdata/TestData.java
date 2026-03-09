d39e017bcd09b172206c0aaef0c280ef4102e8e4
Max Nikandrov <mkss1m.96@gmail.com>
2026-03-09 21:54:59 +0300

    Домашнее задание к 4-му уроку

---
+++ src/test/java/tests/testdata/TestData.java
@@ -0,0 +1,20 @@
+package tests.testdata;
+
+public class TestData {
+    public static String firstName = "David";
+    public static String lastName = "Beckham";
+    public static String userEmail = "davidbeckham@example.ru";
+    public static String sex = "Male";
+    public static String userNumber = "8800555353";
+    public static String day = "02";
+    public static String month = "May";
+    public static String year = "1975";
+    public static String subjects = "English";
+    public static String hobbies = "Sports";
+    public static String address = "London";
+    public static String state = "NCR";
+    public static String city = "Delhi";
+    public static String full_name = firstName + " " + lastName;
+    public static String full_date = day + " " + month + "," + year;
+    public static String state_city = state + " " + city;
+}
